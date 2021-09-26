bb4d04c10bc296686a54b1ab35cef7e947a068db
Burak Aksu <sabburak26@gmail.com>
2021-09-25 20:35:39 -0400

    set up redux in categoryMenu

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -14,13 +14,18 @@ import { QUERY_PRODUCTS } from '../utils/queries';
 import { idbPromise } from '../utils/helpers';
 import spinner from '../assets/spinner.gif';
 
+import { useDispatch, useSelector } from 'react-redux';
+
 function Detail() {
-  const [state, dispatch] = useStoreContext();
+  const dispatch = useDispatch();
+
   const { id } = useParams();
 
   const [currentProduct, setCurrentProduct] = useState({});
 
   const { loading, data } = useQuery(QUERY_PRODUCTS);
+
+  const { state } = useSelector((state) => state);
 
   const { products, cart } = state;
 
